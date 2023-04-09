405e89803a50c2b51a26b719ebfecc742d170b0b
henrykoz <h3nryk.ozog@gmail.com>
2023-04-09 10:02:54 +0100

    Random num generator function added

--- message_generator.js
+++ message_generator.js
@@ -1,7 +1,20 @@
 //Inspirational Message Generator by HO
 
-// Define arrays containing different parts of the message
-const starters = ["You can do it!", "Keep pushing forward!", "Believe in yourself!"];
-const middles = ["anything is possible.", "you are stronger than you think.", "you have what it takes."];
-const endings = ["Stay focused and motivated!", "Keep up the good work!", "Success is just around the corner!"];
+// Define function with arrays containing different parts of the message
+const messageGenerator = {
+    starters: ["You can do it!", "Keep pushing forward!", "Believe in yourself!"],
+    middles: ["anything is possible.", "you are stronger than you think.", "you have what it takes."],
+    endings: ["Stay focused and motivated!", "Keep up the good work!", "Success is just around the corner!"], 
+}
+
+// Store the generated output message
+let inspirationalMessage = [];
+
+//Generate random number
+function generateRandomNumber(num) {
+    // Gets # from 0 -> num - 1
+    return Math.floor(Math.random() * num)
+  }
+
+//   console.log(generateRandomNumber(3));
 
