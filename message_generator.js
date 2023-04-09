8c8b2e28715bd3c3960de0183d4c4eb6396a49b3
henrykoz <h3nryk.ozog@gmail.com>
2023-04-09 09:41:13 +0100

    Define messages

---
+++ message_generator.js
@@ -0,0 +1,7 @@
+//Inspirational Message Generator by HO
+
+// Define arrays containing different parts of the message
+const starters = ["You can do it!", "Keep pushing forward!", "Believe in yourself!"];
+const middles = ["anything is possible.", "you are stronger than you think.", "you have what it takes."];
+const endings = ["Stay focused and motivated!", "Keep up the good work!", "Success is just around the corner!"];
+
